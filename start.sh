f3a0d22119fbe669877c76e6335b337cf7a892fe
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2020-10-02 09:44:36 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+/root/wlp/bin/server start server1 --clean 
+
+curl http://127.0.0.1:10200/daytrader/config?action=resetTrade &
+sleep 10
+
+cd /root/apache-jmeter-3.0/bin; ./jmeter -n -t daytrader7.jmx -JHOST=127.0.0.1 -JPORT=10200 -JTHREADS=2 -j /tmp/jmeter.log -JDURATION=60 &
+sleep 70
+
+Threads=5
+cd /root/apache-jmeter-3.0/bin; ./jmeter -n -t daytrader7.jmx -JHOST=127.0.0.1 -JPORT=10200 -JTHREADS=$Threads -j /tmp/jmeter.log -JDURATION=120 &
+sleep 130
+
+Run_Time=30
+cd /root/apache-jmeter-3.0/bin; ./jmeter -n -t daytrader7.jmx -JHOST=127.0.0.1 -JPORT=10200 -JTHREADS=$Threads -JDURATION=$Run_Time 
+
